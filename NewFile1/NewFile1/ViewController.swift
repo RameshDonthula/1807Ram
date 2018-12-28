25b3036138d5d3720b2b34809ec145f2eb00eca7
Ramesh <rameshramesh1234567@achiever-101.local>
2018-12-28 10:32:25 +0530

    Enter all Files

---
+++ NewFile1/NewFile1/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  NewFile1
+//
+//  Created by Ramesh on 12/28/18.
+//  Copyright © 2018 Ramesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        create()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func create(){
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
